666ca34bef28d3f5bef4135afaef43d59f7b61ee
Charlotte Micheloud <Charlotte.Micheloud@sakk.ch>
2025-03-25 18:31:36 +0100

    * adding statistics types * adding argument `by` and starting with p-value

--- R/summaryTable-continuous.R
+++ R/summaryTable-continuous.R
@@ -2,26 +2,46 @@
 #'
 #' Produces a summary table for continuous data
 #'
+#' @param continuous Summary statistic to display for continuous variables. Options
+#' include "median_IQ" (default), "median_range", "mean_sd".
+#'
+#' @param by A single column from data.
+#' Summary statistics will be stratified by this variable. Default is NULL.
+#' (from tbl_summary)
 #'
 #'
 #'
-#'
-#' @import
+#' @importFrom gtsummary tbl_summary
+#' @import cardx
 #' @export
 
 
-summaryTable <- function(data, continuous = "mean_sd"){
+summaryTable <- function(data, by = NULL, continuous = "median_IQ"
+                         # test_continuous =
+                           ){
 
 if(continuous == "mean_sd"){
   cont <- "{mean} ({sd})"
 }
 
+if(continuous == "median_range"){
+  cont <- "{median} ({min}, {max})"
+}
+
+if(continuous == "median_IQ") {
+  cont <- "{median} ({p25}, {p75})"
+}
+
   # ... to be continued
 
-tbl_summary(data = data,
+tbl <- tbl_summary(data = data, by = by,
             statistic = list(
               all_continuous() ~ cont)
 )
+if(!is.null(by)) {
+  tbl|>
+  add_p(pvalue_fun = label_style_pvalue(digits = 2)) # I am here
+}
 
 
 }
